6b2fc573ec1dc021339d3f41ac8841e040e24eeb
Narayan mishra <83896806+narayanmanish@users.noreply.github.com>
2023-03-19 01:16:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(express.json())
 
 app.use('/api/details',Router)
 
-app.use(express.static(path.join(__dirname,'../client/bulid')))
-app.get('*',(req,res) => res.sendFile(path.join(__dirname,'../client/bulid/index.html')))
+app.use(express.static(path.join(__dirname,'./client/bulid')))
+app.get('*',(req,res) => res.sendFile(path.join(__dirname,'./client/bulid/index.html')))
 
 
 app.listen(port, () => {
